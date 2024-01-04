a6ac02a788a12a9b90bee4f257fa7a025d5d0773
shinthanthtun00 <shinthanthtun00@gmail.com>
2024-01-04 22:47:07 +0630

    Add some notes

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,7 +2,18 @@ package greetings
 
 import "fmt"
 
+// at func Hello (name string) string .
+// name = parameter variable
+// string = parameter variable type
+// another string is return type
+
 func Hello(name string) string {
+
+	// := is a operator used as shortcut for declaring and initializing a variable in one line
+	// if we don't use := we will type like
+	// var message string
+	// message = fmt.Sprintf("Hello, %v . Welcome")
+
 	message := fmt.Sprintf("Hi, %v. Welcome!" , name);
 	return message;
 }
