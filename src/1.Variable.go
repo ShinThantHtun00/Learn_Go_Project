5b029121c3af058f8d512074026c6c9191a9d322
shinthanthtun00 <shinthanthtun00@gmail.com>
2024-01-06 23:36:36 +0630

    Two variable at one sentence

--- src/1.Variable.go
+++ src/1.Variable.go
@@ -11,19 +11,20 @@ func main() {
 	// rune (alias for int32, represents a unicode code point)
 	// float32 float64
 	// complex64 complex128
-	SuccessGuy := "Shin Thant Htun" //string
-	i := 42                         //int
-	f := 3.14                       //float
-	g := 0.867 + 0.5i               //complex
-	var isIt = true                 //bool
-	fmt.Print(i)
-	fmt.Print(f)
-	fmt.Print(g)
-	fmt.Print(isIt)
-	fmt.Printf("The type of i is %T", i)
-	fmt.Printf("The type of f is %T", f)
-	fmt.Printf("The type of g is %T", g)
-	fmt.Printf("The type of isIt is %T", isIt)
-	fmt.Printf("The future most successful guy in the world is %v", SuccessGuy)
-
+	// SuccessGuy := "Shin Thant Htun" //string
+	// i := 42                         //int
+	// f := 3.14                       //float
+	// g := 0.867 + 0.5i               //complex
+	// var isIt = true                 //bool
+	// fmt.Print(i)
+	// fmt.Print(f)
+	// fmt.Print(g)
+	// fmt.Print(isIt)
+	// fmt.Printf("The type of i is %T", i)
+	// fmt.Printf("The type of f is %T", f)
+	// fmt.Printf("The type of g is %T", g)
+	// fmt.Printf("The type of isIt is %T", isIt)
+	// fmt.Printf("The future most successful guy in the world is %v", SuccessGuy)
+	averageOpenRate, displayMessage := 0.23, "Hello"
+	fmt.Println(averageOpenRate, displayMessage)
 }
